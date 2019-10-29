976aba92e9e0949d8e1946568223449f82d77d48
Angel <walker.angel@gmail.com>
2019-10-29 11:23:41 -0400

    adds capstone controller actions

--- app/controllers/api/capstones_controller.rb
+++ app/controllers/api/capstones_controller.rb
@@ -1,6 +1,50 @@
 class Api::CapstonesController < ApplicationController
   def index
-    render json: {message: "sanity check"}
+    @capstones = Capstone.all
+    render "index.json.jb"
+    
+  end
+
+  def create
+    @capstone = Capstone.new(
+      name: params[:name],
+      description: params[:description],
+      url: params[:url],
+      screenshot: params[:screenshot],
+      student_id: params[:student_id]
+      )
+
+    if @capstone.save
+      render "show.json.jb"
+    else
+      render json: {errors: @capstone.errors.full_messages}, status: :bad_request
+    end  
+  end
+
+  def show
+    @capstone = Capstone.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @capstone = Capstone.find_by(id: params[:id])
+
+    @capstone.name = params[:name] || @capstone.name
+    @capstone.description = params[:description] || @capstone.description    
+    @capstone.url = params[:url] || @capstone.url
+    @capstone.screenshot = params[:screenshot] || @capstone.screenshot
+    @capstone.student_id = params[:student_id] || @capstone.student_id
+
+    @capstone.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    @capstone = Capstone.find_by(id: params[:id])
+    capstone.destroy
+    render json: { message: "Your capstone project has been removed from the database" }
     
   end
 end
+
+
