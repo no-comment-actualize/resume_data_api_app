c3254d20c1b48a88bdcd0913532ab2d9755efdf2
benjisp <54284862+benjisp@users.noreply.github.com>
2019-10-28 20:22:01 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   namespace :api do
-   get "/students" => "students#index"
+    get "/students" => "students#index"
     post "/students" => "students#create"
     get "/students/:id" => "students#show"
     patch "/students/:id" => "students#update"
